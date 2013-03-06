6e079546c925dfa1e673d033637c8b437c574e42
sbkgladiator <g.gladiator@tiscali.it>
2013-03-06 15:09:35 +0100

    message

--- 2013-3-6/exercise01.js
+++ 2013-3-6/exercise01.js
@@ -3,7 +3,8 @@ var s=" ";
 for (j = 1; j < 11; j +=1) {
 for (i = 1; i < 11; i +=1) {
 s=s+ "  ";
-if(i*j<10)s=s+" ";
+if(i===1 && i*j<10)s=s+" ";
 s=s+i*j;
+if(i>1 && i*j<10)s=s+" ";
 }s=s+"\n ";}
   console.log(s);
